5b93c45a12239d799d137612a3bd0a9ccdfb9007
George Markham <markhamg7@gmail.com>
2018-02-19 20:29:09 +0000

    Created blockchain class

--- blockchain.py
+++ blockchain.py
@@ -2,4 +2,21 @@ class Blockchain(object):
     def __init__(self):
         self.chain=[]
         self.current_transactions = []
-        
+    def create_block(self):
+        # 1. Create a new block
+        # 2. Add block to the chain
+        pass
+    def create_transaction(self):
+        # 1. Create a new transaction
+        # 2. Add transaction to transaction list
+        pass
+    
+    @staticmethod
+    def generate_hash(block):
+        #Hash a block
+        pass
+    
+    @property
+    def last_block(self):
+        # Return the last block in the chain
+        pass
